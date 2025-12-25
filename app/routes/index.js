bfe6a0ecccd729591333109ee1456413e618317f
royzerbib <zerbibroy@gmail.com>
2025-12-25 14:03:42 +0200

    commented line 72 in routes/index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -69,7 +69,7 @@ const index = (app, db) => {
     // Handle redirect for learning resources link
     app.get("/learn", isLoggedIn, (req, res) => {
         // Insecure way to handle redirects by taking redirect url from query string
-        return res.redirect(req.query.url);
+        //return res.redirect(req.query.url);
     });
 
     // Research Page
