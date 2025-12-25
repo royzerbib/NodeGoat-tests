632ed8c00d7611a6a885e8e928fa69144458f5ca
royzerbib <zerbibroy@gmail.com>
2025-12-25 12:41:38 +0200

    eliminated rows 32-34

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -29,9 +29,9 @@ console.log('abc')
 
         /*jslint evil: true */
         // Insecure use of eval() to parse inputs
-        const preTax = eval(req.body.preTax);
+        /* const preTax = eval(req.body.preTax);
         const afterTax = eval(req.body.afterTax);
-        const roth = eval(req.body.roth);
+        const roth = eval(req.body.roth); */
 
         /*
         //Fix for A1 -1 SSJS Injection attacks - uses alternate method to eval
